3822e2b52affb2cc4e0b9f9d2fbf74b9ff37203d
da-mckeel <damckeel13@gmail.com>
2025-12-28 07:58:27 -0600

    Add clock and delta time

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -20,6 +22,8 @@ def main():
         background_color = (255, 0, 0)
         screen.fill(background_color)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()

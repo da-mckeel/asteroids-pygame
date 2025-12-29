003c0df1ff2e0de875a040dbbb4501c1c0503ab2
da-mckeel <damckeel13@gmail.com>
2025-12-29 00:05:58 -0600

    Add collision of player and asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -33,6 +34,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for ast in asteroids:
+            if ast.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         background_color = "black"
         screen.fill(background_color)
